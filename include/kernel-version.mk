40f50997473d289c3a68f49556d29b36810fa88b
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2022-09-02 13:19:11 +0200

    Update kernel-version.mk

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -6,13 +6,13 @@ ifdef CONFIG_TESTING_KERNEL
   KERNEL_PATCHVER:=$(KERNEL_TESTING_PATCHVER)
 endif
 
-# LINUX_VERSION-5.4 = .89
+LINUX_VERSION-5.4 = .86
 
-# LINUX_KERNEL_HASH-5.4.89 = 595a93c85be1c9a5a65ddd9692dd09a11d44e49a50cace00f4041dc3e8a35406
+LINUX_KERNEL_HASH-5.4.86 = eb36b5fc6ef7b953acba0a3e62d872e0330c4d34b38d58f5714493a4fe3b0e8b
 
-LINUX_VERSION-5.4 = .164
+LINUX_VERSION-4.14 = .248
 
-LINUX_KERNEL_HASH-5.4.164 = 8cfc12ace17ee26a3405a3a356096c1a
+LINUX_KERNEL_HASH-4.14.248 = c993eecf5cc41acbd874fde56eecb87ac021df477defe3d8bd1d811548d3b654
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
